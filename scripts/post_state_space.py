8691624d969d3a5e1e2d1ffbabdd4c30399a6671
Christophe <christophe@imada.sdu.dk>
2021-07-28 15:27:24 +0200

    Added load_state_space() funciton

---
+++ scripts/post_state_space.py
@@ -0,0 +1,28 @@
+from mechsearch.grammar import Grammar
+import scripts.rhea_analysis.util as util
+from data.rhea.db import RheaDB
+from mechsearch.state_space import StateSpace, Path
+from scripts.announcements import *
+import json
+
+
+def load_state_sapce(rhea_id: str, state_space_path: str, dg_path: str,
+                     aa_loc: str = "data/amino_acids.json", verbose: int = 0):
+    message("Loading grammar", verbose=verbose, verbose_level_threshold=2)
+    grammar_aminos = Grammar()
+    grammar_aminos.load_file(aa_loc)
+    grammar_rules = grammar_aminos + util.load_rules()
+    rhea_db = RheaDB()
+    reaction = rhea_db.get_reaction(rhea_id)
+    grammar_reaction = util.reaction2grammar(reaction)
+    grammar = grammar_rules + grammar_reaction
+
+    message("Loading state space", verbose=verbose, verbose_level_threshold=2)
+    with open(state_space_path) as f:
+        state_space = StateSpace.from_json(json.load(f), grammar, dg_path)
+    message(f"Analyzing StateSpace(|V| = {state_space.number_of_states}, |E| = {len(state_space.graph.edges)})",
+            verbose=verbose, verbose_level_threshold=2)
+    message(f"\t DG(|V| = {state_space.derivation_graph.numVertices}, |E| = {state_space.derivation_graph.numEdges})",
+            verbose=verbose, verbose_level_threshold=2)
+    state_space.freeze()
+    return state_space
